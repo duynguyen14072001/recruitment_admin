9969b3186bd73eb76e8577de89d506fd0e651188
Duy Nguyen <duy.nguyenanh01@gmail.com>
2024-11-10 21:13:31 +0700

    add layout

--- src/helpers/utils/helper.ts
+++ src/helpers/utils/helper.ts
@@ -1,41 +1,41 @@
 export const emptyObj = (object: Object) => {
-    return !Object.keys(object).length
+  return !Object.keys(object).length
 }
 
 export const findData = (arr: any, id: any) => {
-    if (arr.length) return arr.find((item: any) => item.id == id)
-    return []
+  if (arr.length) return arr.find((item: any) => item.id == id)
+  return []
 }
 
 export const findIndex = (arr: any, index: number): number => {
-    return arr.findIndex((item: any) => item.index === index)
+  return arr.findIndex((item: any) => item.index === index)
 }
 
 export const getOptions = (array: any[] | undefined, name = 'name') => {
-    return array?.map((item) => ({ label: item[name], value: item.id }))
+  return array?.map((item) => ({ label: item[name], value: item.id }))
 }
 
 export const getObjOptions = (obj: Record<number, string>, hasAll = true, end = true) => {
-    const options = Object.entries(obj).map(([value, label]) => ({
-        label,
-        value: +value,
-    }))
+  const options = Object.entries(obj).map(([value, label]) => ({
+    label,
+    value: +value
+  }))
 
-    if (hasAll && end) return [...options, ALL_OPTION]
-    if (hasAll) return [ALL_OPTION, ...options]
+  if (hasAll && end) return [...options, ALL_OPTION]
+  if (hasAll) return [ALL_OPTION, ...options]
 
-    return options
+  return options
 }
 
 export const mapKeyToData = (value: any) => {
-    return Object.entries(value).map(([k, v]) => {
-        return Object.fromEntries([
-            ['key', k],
-            ['data', Array.isArray(v) ? JSON.stringify((v as Array<string>).map((i) => +i)) : v],
-        ])
-    })
+  return Object.entries(value).map(([k, v]) => {
+    return Object.fromEntries([
+      ['key', k],
+      ['data', Array.isArray(v) ? JSON.stringify((v as Array<string>).map((i) => +i)) : v]
+    ])
+  })
 }
 
 export const getFirstErrorMessage = (errors: Record<string, any>) => {
-    return Object.values(errors).flat().shift() as string
+  return Object.values(errors).flat().shift() as string
 }
--- src/helpers/utils/helperUI.ts
+++ src/helpers/utils/helperUI.ts
@@ -1,66 +1,66 @@
 import { h } from 'vue'
 import { notification } from 'ant-design-vue'
 import {
-    CheckCircleOutlined,
-    InfoCircleOutlined,
-    WarningOutlined,
-    CloseCircleOutlined,
+  CheckCircleOutlined,
+  InfoCircleOutlined,
+  WarningOutlined,
+  CloseCircleOutlined
 } from '@ant-design/icons-vue'
 import { REGEX_EMAIL, STATUS_CODE_SUCCESS } from '..'
 import i18n from '@/lang'
 
 const { t } = i18n
 const typeIcon = {
-    info: [InfoCircleOutlined, 'blue'],
-    success: [CheckCircleOutlined, 'green'],
-    warning: [WarningOutlined, 'orange'],
-    error: [CloseCircleOutlined, 'red'],
+  info: [InfoCircleOutlined, 'blue'],
+  success: [CheckCircleOutlined, 'green'],
+  warning: [WarningOutlined, 'orange'],
+  error: [CloseCircleOutlined, 'red']
 }
 
 export type notifyType = 'success' | 'warning' | 'info' | 'error'
 export const notify = (message?: string, description?: string, type: notifyType = 'success') => {
-    const [icon, color] = typeIcon[type]
-    notification[type]({
-        message,
-        description,
-        duration: 3,
-        placement: 'topRight',
-        icon: () => h(icon, { style: `color: ${color}` }),
-    })
+  const [icon, color] = typeIcon[type]
+  notification[type]({
+    message,
+    description,
+    duration: 3,
+    placement: 'topRight',
+    icon: () => h(icon, { style: `color: ${color}` })
+  })
 }
 
 export const notifyStatus = (status: number, message: string) => {
-    const type = status === STATUS_CODE_SUCCESS ? 'success' : 'error'
-    const mes = status === STATUS_CODE_SUCCESS ? message : 'Error!'
-    return notify(mes, '', type)
+  const type = status === STATUS_CODE_SUCCESS ? 'success' : 'error'
+  const mes = status === STATUS_CODE_SUCCESS ? message : 'Error!'
+  return notify(mes, '', type)
 }
 
 export const trim = (field: string, formState: any) => {
-    if (formState[field]) {
-        formState[field] = formState[field].trim()
-    }
+  if (formState[field]) {
+    formState[field] = formState[field].trim()
+  }
 }
 
 export const trimDynamic = (array: number[] | string[], form: any) => {
-    const [field1, index, field2] = array
-    form[field1][index][field2] = form[field1][index][field2].trim()
+  const [field1, index, field2] = array
+  form[field1][index][field2] = form[field1][index][field2].trim()
 }
 
 export const formatCurrency = (money: string) => {
-    const result = unRoundFixed(money)
-    return String(result).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  const result = unRoundFixed(money)
+  return String(result).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
 
 export const trimAndFormatCurrency = (field: string | number, formState: any) => {
-    if (formState[field]) {
-        formState[field] = formState[field].trim()
-        formState[field] = formatCurrency(formState[field])
-    }
+  if (formState[field]) {
+    formState[field] = formState[field].trim()
+    formState[field] = formatCurrency(formState[field])
+  }
 }
 
 export const unRoundFixed = (num: string) => {
-    const unformattedNum = num.replace(/,/g, '')
-    return parseFloat(unformattedNum)
+  const unformattedNum = num.replace(/,/g, '')
+  return parseFloat(unformattedNum)
 }
 
 export const formatter = (value: string) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
@@ -68,25 +68,25 @@ export const formatter = (value: string) => `${value}`.replace(/\B(?=(\d{3})+(?!
 export const parser = (value: string) => value!.replace(/\$\s?|(,*)/g, '')
 
 export const validateEmail = async (_: any, value: any, column: string) => {
-    const regex = new RegExp(REGEX_EMAIL)
-    if (value) {
-        if (!regex.test(value)) {
-            return Promise.reject(
-                new Error(
-                    t('validation.email', {
-                        0: t(column),
-                    }) as string
-                )
-            )
-        }
+  const regex = new RegExp(REGEX_EMAIL)
+  if (value) {
+    if (!regex.test(value)) {
+      return Promise.reject(
+        new Error(
+          t('validation.email', {
+            0: t(column)
+          }) as string
+        )
+      )
     }
-    return Promise.resolve()
+  }
+  return Promise.resolve()
 }
 
 export const getFormikErr = (data: any) => {
-    const errorData: any = {}
-    data.forEach((v: any, k: any) => {
-        errorData[k] = v + ''
-    })
-    return errorData
+  const errorData: any = {}
+  data.forEach((v: any, k: any) => {
+    errorData[k] = v + ''
+  })
+  return errorData
 }
